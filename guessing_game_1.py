5ec5fa8cdd4c44c02d1b0fb9f22ebf477542e595
sorei9240 <139683155+sorei9240@users.noreply.github.com>
2023-09-15 17:25:07 +0100

    "added replay function"

--- guessing_game_1.py
+++ guessing_game_1.py
@@ -1,26 +1,43 @@
+#import random module
 import random
-
+#define guessing game function
 def guessing_game():
-    secret_number = random.randint(1, 20)
-    num_guesses = 0
-    
-    while True:
-        user_guess = int(input("Pick a number between 1 and 20. "))
-        if user_guess > secret_number:
-            num_guesses += 1
-            if num_guesses > 5:
-                print("You lose")
-                break
-            print("Too high, try again.")
+    print("Welcome to the number guessing game. You have 5 tries to guess the correct number.")
+    secret_number = random.randint(1, 20) #generates secret number
+    num_guesses = 0 #counter variable for attempts
+    remaining = 5 #counter variable for remaining attempts
 
-        elif user_guess < secret_number:
-            num_guesses += 1
-            if num_guesses > 5:
-                print("You lose")
-                break
-            print("Too low, try again")
-        else:
-            print(f"That's right! The secret number was {secret_number}. You got it in {num_guesses} tries.")
-            break
+    while True:
+        #user prompted to guess a number
+        user_guess = int(input("Pick a whole number between 1 and 20. ")) 
+        if user_guess > secret_number: 
+            num_guesses += 1 #put attempt counter up
+            remaining += -1 #subtract from remaining attempts
+            if num_guesses > 4: #checks to see if user has attempts left
+                print("You lose, better luck next time.")
+                break #stops program because user has no remaining attempts
+            print(f"Too high, try again. You have {remaining} attempt(s) left.") 
+
+        elif user_guess < secret_number: 
+            num_guesses += 1 
+            remaining += -1 
+            if num_guesses > 4: 
+                print("You lose, better luck next time.")
+                break 
+            print(f"Too low, try again. You have {remaining} attempt(s) left.") 
+
+        else: 
+            print(f"That's right! The secret number is {secret_number}. You got it in {num_guesses + 1} attempt(s).") #displays secret number and attempts
+            break #stops program because user has won
 
 guessing_game()
+
+def play_again():
+    while True:
+        replay = input("Would you like to play again? (yes/no) ").lower()
+        if replay == "yes":
+            guessing_game()
+        else:
+            print("Thanks for playing!")
+            break
+play_again()
