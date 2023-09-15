b19112d0cc4c0b2a60681170822ebd2b57ed8855
sorei9240 <139683155+sorei9240@users.noreply.github.com>
2023-09-15 12:19:32 +0100

    added lose function

--- guessing_game_1.py
+++ guessing_game_1.py
@@ -8,10 +8,17 @@ def guessing_game():
         user_guess = int(input("Pick a number between 1 and 20. "))
         if user_guess > secret_number:
             num_guesses += 1
+            if num_guesses > 5:
+                print("You lose")
+                break
             print("Too high, try again.")
+
         elif user_guess < secret_number:
-            print("Too low, try again")
             num_guesses += 1
+            if num_guesses > 5:
+                print("You lose")
+                break
+            print("Too low, try again")
         else:
             print(f"That's right! The secret number was {secret_number}. You got it in {num_guesses} tries.")
             break
